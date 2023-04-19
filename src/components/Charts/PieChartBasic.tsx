ed078ce12dac99eb2302b3f07147dfcf62e9e611
MartinPucan <martin.pucan@rixo.cz>
2023-04-19 21:02:50 +0200

    disabled chart render with empty data

--- src/components/Charts/PieChartBasic.tsx
+++ src/components/Charts/PieChartBasic.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PieChart, Pie, Cell, BarChart, Bar, BarProps } from "recharts";
+import { PieChart, Pie, Cell, BarChart, Bar, BarProps } from 'recharts';
 import { Box, Container, Paper } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { getFormattedData, getValuesOfIncomesAndExpenses } from '../../utils';
@@ -38,53 +38,59 @@ export const PieChartBasic: React.FC<IProps> = ({ data }) => {
   return (
     <>
       <Container sx={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', gap: '1rem' }}>
-        <Paper elevation={1} sx={{ padding: '1rem' }}>
-          <Typography variant="h5">Income</Typography>
-          <PieChart width={250} height={250}>
-            <Pie
-              data={incomeData}
-              cx={125}
-              cy={125}
-              labelLine={false}
-              label={renderCustomizedLabel}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="amount"
-            >
-              {incomeData.map((entry: any, index: number) => (
-                <Cell key={`cell-${entry.id}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-          </PieChart>
-        </Paper>
-        <Paper elevation={1} sx={{ padding: '1rem' }}>
-          <Typography variant="h5">Expenses</Typography>
-          <PieChart width={250} height={250}>
-            <Pie
-              data={expensesData}
-              cx={125}
-              cy={125}
-              labelLine={false}
-              label={renderCustomizedLabel}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="amount"
-            >
-              {expensesData.map((entry: any, index: number) => (
-                <Cell key={`cell-${entry.id}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-          </PieChart>
-        </Paper>
-        <Paper elevation={1} sx={{ padding: '1rem' }}>
-          <Typography variant="h5">Income / Expenses</Typography>
-          <Box sx={{ display: 'grid', placeItems: 'center', marginTop: '3rem' }}>
-            <BarChart width={150} height={150} data={totalValues}>
-              {/* @ts-ignore */}
-              <Bar dataKey="value" fill={ (bar: BarProps<ChartData>) => bar.fill || '#98D8AA' } />
-            </BarChart>
-          </Box>
-        </Paper>
+        {incomeData.length !== 0 && (
+          <Paper elevation={1} sx={{ padding: '1rem' }}>
+            <Typography variant="h5">Income</Typography>
+            <PieChart width={250} height={250}>
+              <Pie
+                data={incomeData}
+                cx={125}
+                cy={125}
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="amount"
+              >
+                {incomeData.map((entry: any, index: number) => (
+                  <Cell key={`cell-${entry.id}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+            </PieChart>
+          </Paper>
+        )}
+        {expensesData.length !== 0 && (
+          <Paper elevation={1} sx={{ padding: '1rem' }}>
+            <Typography variant="h5">Expenses</Typography>
+            <PieChart width={250} height={250}>
+              <Pie
+                data={expensesData}
+                cx={125}
+                cy={125}
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="amount"
+              >
+                {expensesData.map((entry: any, index: number) => (
+                  <Cell key={`cell-${entry.id}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+            </PieChart>
+          </Paper>
+        )}
+        {incomeData.length !== 0 && expensesData.length !== 0 && (
+          <Paper elevation={1} sx={{ padding: '1rem' }}>
+            <Typography variant="h5">Income / Expenses</Typography>
+            <Box sx={{ display: 'grid', placeItems: 'center', marginTop: '3rem' }}>
+              <BarChart width={150} height={150} data={totalValues}>
+                {/* @ts-ignore */}
+                <Bar dataKey="value" fill={(bar: BarProps<ChartData>) => bar.fill || '#98D8AA'} />
+              </BarChart>
+            </Box>
+          </Paper>
+        )}
       </Container>
     </>
   );
